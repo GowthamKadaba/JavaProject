c9cc5fb529975c82cd74dd66b0fe9aab0838523b
gowthamkadaba <gowthamkadaba@yahoo.co.in>
2018-12-07 17:34:41 -0500

    Adding programCreek

--- src/programCreek/validPalindrome.java
+++ src/programCreek/validPalindrome.java
@@ -2,9 +2,32 @@ package programCreek;
 
 public class validPalindrome {
 
+    public static boolean checkValidPalin(String str){
+
+        if (str == null || str.length() ==1){
+            return false;
+        }
+
+        str = str.replaceAll("[^a-bA-B0-9]","");
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) != str.charAt(str.length() -i) ){
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
 
 
     public static void main(String[] args){
-
+        String s = "TT HH!Q TT";
+        if(checkValidPalin(s)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("its Not");
+        }
     }
 }
